7cf3ee7abd05b8fb5e6ea1d40ec524d253a8188a
gedeon kakulia <115647528+Milltiades@users.noreply.github.com>
2022-12-05 16:41:08 +0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,42 @@
+let container = document.getElementById('container');
+let wave = document.getElementById('wave');
+let about = document.getElementById('about')
+
+
+
+
+function scrollAbout() {
+    window.scrollBy(0,1000);
+}
+
+
+function scrollProjects(){
+    window.scrollBy(0,1900);
+}
+
+function scrollContact(){
+    window.scrollBy(0,3700);
+}
+
+
+function startIt(){
+    setTimeout(()=>{
+        container.style.visibility = 'visible'
+        wave.style.visibility = 'hidden'
+    },3000)
+}
+startIt();
+
+
+function btnfunc1() {
+    window.open('https://milltiades.github.io/equalizer-landing-page/')
+}
+
+
+function btnfunc2() {
+    window.open('https://milltiades.github.io/typemaster-pre-launch-landing-page/')
+}
+
+function btnfunc3() {
+    window.open('https://milltiades.github.io/four-card-feature-section-master/')
+}
